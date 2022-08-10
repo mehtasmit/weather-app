5933e45239d8f705cc98a334ac19c4d1f913090d
Smit Mehta <mehtasmit001@gmail.com>
2022-08-11 00:41:30 +0530

    Search UI + Search functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,12 +11,22 @@ export class AppComponent implements OnInit {
   constructor(private weatherService: WeatherService) {}
 
   weatherData?: WeatherData;
+  cityName:string = 'Jamnagar';
   ngOnInit(): void {
-      this.weatherService.getWeatherData('Jamnagar').subscribe({
+    this.getWeatherData(this.cityName);
+    this.cityName = '';
+  }
+
+  onSubmit() {
+    this.getWeatherData(this.cityName);
+    this.cityName = '';
+  }
+  
+  private getWeatherData(cityName:string) {
+      this.weatherService.getWeatherData(cityName).subscribe({
         next: (response) => {
-         this.weatherData = response;
+        this.weatherData = response;
         }
       })
   }
-
 }
